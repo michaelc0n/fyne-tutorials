7ecf291c4df395df7dbaa9e71023a80dab58265c
Michael <michaeljsaenz@gmail.com>
2022-11-21 13:46:38 -0600

    checkbox widget

---
+++ checkbox/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/widget"
+)
+
+func main() {
+
+	// create a new app
+	app := app.New()
+
+	// create a new window
+
+	win := app.NewWindow("My New Title") // use any title for app
+
+	// resize fyne app window
+	win.Resize(fyne.NewSize(400, 400)) // first width, then height
+
+	// checkbox widget
+	checkBox := widget.NewCheck("True/False, select checkbox", func(b bool) {
+		fmt.Printf("my checkbox value %t\n", b)
+		// when checkbox selected (true), exit fyne app
+		if b {
+			app.Quit()
+		}
+	})
+	// set content eith checkBox
+	win.SetContent(checkBox)
+	win.ShowAndRun() // run app
+}
